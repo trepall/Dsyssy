f09cc5203932ece76103ec1014392dd2736ef291
cherry <pupsiclol0808@gmail.com>
2025-11-17 00:19:15 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    CRYPTO_BOT_TOKEN = "488475:AAgqVcoJeiMg1FN6nYxMI3iIlDTXBfy6PUF"
+    CRYPTO_BOT_HEADER = "crypto_webhook_secret_2024"
+    DATABASE_URL = "postgresql://postgres:fEhfug-zefcek-9wisde@db.xbkczcrtmbiescqwylkf.supabase.co:5432/postgres"
+    TON_WALLET = "UQA8t_PXSXu1mfNdwnLVS7BAv4WV6d-L8A2BTn7LA8XL2D-G"
+    SECRET_KEY = "supabase_crypto_bot_secret_key_2024_v2"
